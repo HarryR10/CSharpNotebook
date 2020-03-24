e3152416ab4a173e2551a816c26d1af8fb6b5308
igor_spirin <igor.r10@bk.ru>
2020-03-24 23:19:28 +0300

    no message

---
+++ TasksAndParallel/test2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Chapter1
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task t = Task.Run(() =>
+            {
+                for (int x = 0; x < 100; x++)
+                {
+                    Console.Write('*');
+                }
+            });
+            t.Wait();
+
+            Task<int> t2 = Task.Run(() =>
+            {
+                return 42;
+            });
+            Console.WriteLine(t2.Result); // Displays 42
+
+            Task<int> t3 = Task.Run(() =>
+            {
+                return 42;
+            });
+
+            t3.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            t3.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var completedTask = t3.ContinueWith((i) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTask.Wait();
+
+            Task<int>[] tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+            while (tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> anotherCompletedTask = tasks[i];
+
+                Console.WriteLine(anotherCompletedTask.Result);
+
+                // тут происходит очистка массива задач
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
+
+            ParallelLoopResult result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) =>
+            {
+                Console.WriteLine(i);
+                if (i == 500)
+                {
+                    Console.WriteLine("Breaking loop");
+                    loopState.Break();
+                }
+                return;
+            });
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
